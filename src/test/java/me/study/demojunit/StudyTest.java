0b5c1b26472b6d997f61f3fcfa27d08110f81002
showpointer <wanho92@gmail.com>
2020-02-02 21:59:04 +0900

    테스트 반복 실습

--- src/test/java/me/study/demojunit/StudyTest.java
+++ src/test/java/me/study/demojunit/StudyTest.java
@@ -18,10 +18,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StudyTest {
@@ -76,6 +80,21 @@ public class StudyTest {
 		
 		System.out.println("실행 되지 않음");
 	}
+	
+	@DisplayName("테스트 반복")
+	@RepeatedTest(10)
+	void repeatTest(RepetitionInfo repetitionInfo) {
+		System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" + 
+					repetitionInfo.getTotalRepetitions());
+	}
+	
+	@DisplayName("파라미터 변경 테스트")
+	@ParameterizedTest(name = "{index} {displayName} message = {0}")
+	@ValueSource(strings = {"날씨가", "많이", "춥습니다"})
+	void parameterizedTest(String message) {
+		System.out.println(message);
+	}
+	
 			
 	@Test
 	void create_new_study() {
